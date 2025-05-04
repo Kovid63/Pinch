7f46422724a63b6f59312add4d4dde596c4c61ca
Sarvansh Pachori <sarvansh.pachori45@gmail.com>
2025-05-05 03:48:51 +0530

    styling left for searchBar with tiles for items

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,54 +1,70 @@
 import { useEffect, useState } from "react";
 import api_searchBar from "../api/get_searchBar";
 
-function SearchBar(){
+function SearchBar() {
+  const [search_text, setSearch_text] = useState("");
 
-    const [search_text,setSearch_text] = useState("");
+  // const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e)=>{
+  //     setSearch_text(e.target.value);
+  // }
 
-    // const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e)=>{
-    //     setSearch_text(e.target.value);
-    // }
+  useEffect(() => {
+    if (!search_text) return;
+    const fetch_data = async () => {
+      try {
+        const response = await api_searchBar.get(`search/multi`, {
+          params: {
+            api_key: import.meta.env.VITE_TMDB_API_KEY,
+            query: search_text,
+            page: 1,
+          },
+        });
 
-    useEffect(()=>{
-        if (!search_text) return;
-        const fetch_data = async ()=>{
-            try{
-                const response = await api_searchBar.get(`search/movie`,{
-                    params:{
-                        api_key:import.meta.env.VITE_TMDB_API_KEY,
-                        query:search_text,
-                        page:1
-                    }
-                });
-               
-                console.log(response.data.results.map( (movie:any)=>{
-                    const arr = {title:movie.title, release_year:movie.release_date.split('-')[0], poster_path:movie.poster_path};
-                    return arr;
-                }
-                ));
-
-            }catch(err){
-                console.log(err);
+        const filtered = response.data.results.filter(
+          (item: any) => item.media_type === "movie" || item.media_type === "tv"
+        );
+        console.log(
+          filtered.map((item: any) => {
+            const arr = {
+              title: "",
+              poster_path: item.poster_path,
+              media_type: item.media_type,
+              vote_average: item.vote_average,
+              year: 0,
+            };
+            if (item.media_type == "tv") {
+              arr["year"] = item.first_air_date.split("-")[0];
+              arr["title"] = item.name;
+            } else {
+              arr["year"] = item.release_date.split("-")[0];
+              arr["title"] = item.title;
             }
-        }
-        fetch_data();
-    }
-        ,[search_text]);
 
-    return(
-        <>
-            <input 
-            type="text" 
-            name="searchBar" 
-            id="searchBar" 
-            value={search_text}
-            onChange={e=>setSearch_text(e.target.value)}
-            placeholder="Type to search..."
-            />
-            <button></button>
-            <p>{search_text}</p>
-        </>
-    )
+            return arr;
+          })
+        );
+
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetch_data();
+  }, [search_text]);
+
+  return (
+    <>
+      <input
+        type="text"
+        name="searchBar"
+        id="searchBar"
+        value={search_text}
+        onChange={(e) => setSearch_text(e.target.value)}
+        placeholder="Type to search..."
+      />
+      <button></button>
+      <p>{search_text}</p>
+    </>
+  );
 }
 
 export default SearchBar;
