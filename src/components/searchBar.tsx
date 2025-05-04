e3d49bfb40adf8b8a6a8f79f4c9f4dbf46e198cd
Sarvansh Pachori <sarvansh.pachori45@gmail.com>
2025-05-04 16:25:35 +0530

    added pages and compoenents folder

---
+++ src/components/searchBar.tsx
@@ -0,0 +1,12 @@
+
+function SearchBar(){
+
+    return(
+        <>
+            <input type="text" name="searchBar" id="searchBar" />
+            <button type="submit"></button>
+        </>
+    )
+}
+
+export default SearchBar;
