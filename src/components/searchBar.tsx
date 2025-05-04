0fb36c296392609520ae44184021337dd08ad285
Sarvansh Pachori <sarvansh.pachori45@gmail.com>
2025-05-04 16:55:41 +0530

    started work on searchBar

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 
 function SearchBar(){
 
+    const [search_text,setSearch_text] = useState("");
+
+    // const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e)=>{
+    //     setSearch_text(e.target.value);
+    // }
+
     return(
         <>
-            <input type="text" name="searchBar" id="searchBar" />
+            <input 
+            type="text" 
+            name="searchBar" 
+            id="searchBar" 
+            value={search_text}
+            onChange={e=>setSearch_text(e.target.value)}
+            placeholder="Type to search..."
+            />
             <button type="submit"></button>
+            <p>{search_text}</p>
         </>
     )
 }
