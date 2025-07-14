0d1e7b4b4a277575117845fd3ea742e9a93190fe
sarvansh30 <sarvansh.pachori45@gmail.com>
2025-07-14 22:26:18 +0530

    created custom hook useDebounce

--- src/components/movieTile.tsx
+++ src/components/movieTile.tsx
@@ -1,16 +1,15 @@
 
- const MovieTile = ()=>{
+ const MovieTile = (props:any)=>{
 
     // Object { title: "Marvel's Ultimate Spider-Man", poster_path: "/jK3pc8XOQT8UgdvSjMFk8xLQOxE.jpg", media_type: "tv", vote_average: 7.676, year: "2012" }
     return(
         <div className="relative bg-gray-800 w-45 h-55 rounded-xl ">
             
-           <img src={`https://image.tmdb.org/t/p/w300/jK3pc8XOQT8UgdvSjMFk8xLQOxE.jpg`}  alt="movie poster" 
+           <img src={`https://image.tmdb.org/t/p/w300/${props.poster_path}`}  alt="movie poster" 
              className="absolute w-44 h-54 mt-0.5 ml-0.5 rounded-xl " />
-            {/* <h4>Movie name</h4> */}
             <div className="absolute  w-44 h-54 bg-black/20 backdrop-saturate-125 backdrop-contrast-100 border-gray-300 shadow-lg rounded-xl "></div>
             <div className="absolute  w-44 bg-white/10 h-54 backdrop-saturate-125 backdrop-contrast-125 border-gray-100 shadow-lg rounded-xl"></div>
-        
+        <p>Hekko</p>
         </div>
     )
 } 
